0beb696a475254e01294ce83e95dc884964f75e3
ryu <ryu000407@gmail.com>
2022-04-23 17:17:56 +0900

    update: variable types

---
+++ type.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val a: Boolean = true
+    val b: Byte = 123
+    val c: Short = 123
+    val d: Int = 123
+    val e: Long = 123L
+    val f: Float = 12.3F
+    val g: Double = 12.3
+    val h: Char = 'A'
+    val i: String = "ABC"
+}
