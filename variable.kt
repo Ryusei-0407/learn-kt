9e2f2cbeac32940abf8da8461be793a5aaeb389a
ryu <ryu000407@gmail.com>
2022-04-23 17:17:44 +0900

    update: defin variables

---
+++ variable.kt
@@ -0,0 +1,20 @@
+const val FOO = "FOO"
+
+fun main() {
+    var a: Int = 42
+    var b = 2
+    var c: Int
+    c = 3
+
+    val MAX_COUNT: Int = 1024
+    val MAX_SIZE = 1024
+
+    println("a = $a")
+    println("b = $b")
+    println("c = $c")
+
+    println("MAX_COUNT = $MAX_COUNT")
+    println("MAX_SIZE = $MAX_SIZE")
+
+    println("FOO = $FOO")
+}
