98238e0bcbb0af93b01b341aae5816d55a1d89a8
ryu <ryu000407@gmail.com>
2022-04-23 18:26:08 +0900

    update: define list

---
+++ list.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val cols = arrayOf("Red", "Green", "Blue", "Yellow", "Pink", "Black", "White", "Orange", "Purple", "Brown")
+
+    println("cols[0]: ${cols[0]}")
+    println("cols[1]: ${cols[1]}")
+    
+    for (c in cols) {
+        println(c)
+    }
+}
