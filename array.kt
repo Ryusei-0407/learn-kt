73618ac1974f3b521e391196f9256068d5fae3e1
ryu <ryu000407@gmail.com>
2022-04-23 18:27:51 +0900

    update: define array

---
+++ array.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val cols: Array<String> = arrayOf("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+
+    println("cols[0] = ${cols[0]}")
+    println("cols[1] = ${cols[1]}")
+
+    for (c in cols) {
+        println(c)
+    }
+}
