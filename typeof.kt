5b6f39c522093868b10c3c2b0b5e2c6c906a5d4b
ryu <ryu000407@gmail.com>
2022-04-23 18:42:45 +0900

    update: type check

---
+++ typeof.kt
@@ -0,0 +1,14 @@
+fun main() {
+    println("ABC" is String)
+    println("ABC" !is String)
+
+    open class ClassA() {}
+    class ClassB(): ClassA() {}
+    val a = ClassA()
+    val b = ClassB()
+
+    println(a is ClassA)
+    println(a is ClassB)
+    println(b is ClassA)
+    println(b is ClassB)
+}
