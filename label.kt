366ef4fe2823e17e56a44afd0133f7a3a5f190d3
ryu <ryu000407@gmail.com>
2022-04-24 22:25:37 +0900

    update: label

---
+++ label.kt
@@ -0,0 +1,9 @@
+fun main() {
+    foo@ for (i in 1..10) {
+        for (j in 1..10) {
+            if (i == 5 && j == 5) break@foo
+                print("${i * j} ")
+        }
+        println()
+    }
+}
